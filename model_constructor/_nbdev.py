5ca63e7d041a0f89cace61e886e02896163df9fe
ayasyrev <jzz@zoo>
2020-01-09 16:54:16 +0300

    first steps

--- model_constructor/_nbdev.py
+++ model_constructor/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"Net": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://ayasyrev.github.io/model_constructor/"
 
