1c0861aa890372e6f8baf881f79db83bccd79388
ayasyrev <a.yasyrev@gmail.com>
2020-11-12 07:01:25 +0000

    cleared code

--- model_constructor/layers.py
+++ model_constructor/layers.py
@@ -5,7 +5,7 @@ __all__ = ['Flatten', 'noop', 'Noop', 'ConvLayer', 'act_fn', 'conv1d', 'SimpleSe
 # Cell
 import torch.nn as nn
 import torch
-from torch.nn.utils import spectral_norm # weight_norm,
+from torch.nn.utils import spectral_norm
 from collections import OrderedDict
 
 # Cell
@@ -13,10 +13,14 @@ class Flatten(nn.Module):
     '''flat x to vector'''
     def __init__(self):
         super().__init__()
-    def forward(self, x): return x.view(x.size(0), -1)
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
 
 # Cell
-def noop(x): return x
+def noop(x):
+    return x
+
 
 class Noop(nn.Module):
     '''Dummy module'''
@@ -29,58 +33,64 @@ class Noop(nn.Module):
 # Cell
 act_fn = nn.ReLU(inplace=True)
 
+
 class ConvLayer(nn.Sequential):
     """Basic conv layers block"""
     Conv2d = nn.Conv2d
-    def __init__(self, ni, nf, ks=3, stride=1,
-            act=True,  act_fn=act_fn,
-            bn_layer=True, bn_1st=True, zero_bn=False,
-            padding=None, bias=False, groups=1, **kwargs):
 
-        if padding==None: padding = ks//2
+    def __init__(self, ni, nf, ks=3, stride=1,
+                 act=True, act_fn=act_fn,
+                 bn_layer=True, bn_1st=True, zero_bn=False,
+                 padding=None, bias=False, groups=1, **kwargs):
+
+        if padding is None:
+            padding = ks // 2
         layers = [('conv', self.Conv2d(ni, nf, ks, stride=stride, padding=padding, bias=bias, groups=groups))]
         act_bn = [('act_fn', act_fn)] if act else []
         if bn_layer:
             bn = nn.BatchNorm2d(nf)
             nn.init.constant_(bn.weight, 0. if zero_bn else 1.)
             act_bn += [('bn', bn)]
-        if bn_1st: act_bn.reverse()
+        if bn_1st:
+            act_bn.reverse()
         layers += act_bn
         super().__init__(OrderedDict(layers))
 
 # Cell
 # SA module from mxresnet at fastai. todo - add persons!!!
-
-#Unmodified from https://github.com/fastai/fastai/blob/5c51f9eabf76853a89a9bc5741804d2ed4407e49/fastai/layers.py
-def conv1d(ni:int, no:int, ks:int=1, stride:int=1, padding:int=0, bias:bool=False):
+# Unmodified from https://github.com/fastai/fastai/blob/5c51f9eabf76853a89a9bc5741804d2ed4407e49/fastai/layers.py
+def conv1d(ni: int, no: int, ks: int = 1, stride: int = 1, padding: int = 0, bias: bool = False):
     "Create and initialize a `nn.Conv1d` layer with spectral normalization."
     conv = nn.Conv1d(ni, no, ks, stride=stride, padding=padding, bias=bias)
     nn.init.kaiming_normal_(conv.weight)
-    if bias: conv.bias.data.zero_()
+    if bias:
+        conv.bias.data.zero_()
     return spectral_norm(conv)
 
 
-# Adapted from SelfAttention layer at https://github.com/fastai/fastai/blob/5c51f9eabf76853a89a9bc5741804d2ed4407e49/fastai/layers.py
+# Adapted from SelfAttention layer at
+# https://github.com/fastai/fastai/blob/5c51f9eabf76853a89a9bc5741804d2ed4407e49/fastai/layers.py
 # Inspired by https://arxiv.org/pdf/1805.08318.pdf
 class SimpleSelfAttention(nn.Module):
-    def __init__(self, n_in:int, ks=1, sym=False):#, n_out:int):
+    def __init__(self, n_in: int, ks=1, sym=False):  # , n_out:int):
         super().__init__()
-        self.conv = conv1d(n_in, n_in, ks, padding=ks//2, bias=False)
+        self.conv = conv1d(n_in, n_in, ks, padding=ks // 2, bias=False)
         self.gamma = nn.Parameter(torch.tensor([0.]))
         self.sym = sym
         self.n_in = n_in
-    def forward(self,x):
+
+    def forward(self, x):
         if self.sym:
             # symmetry hack by https://github.com/mgrankin
-            c = self.conv.weight.view(self.n_in,self.n_in)
-            c = (c + c.t())/2
-            self.conv.weight = c.view(self.n_in,self.n_in,1)
+            c = self.conv.weight.view(self.n_in, self.n_in)
+            c = (c + c.t()) / 2
+            self.conv.weight = c.view(self.n_in, self.n_in, 1)
         size = x.size()
-        x = x.view(*size[:2],-1)   # (C,N)
+        x = x.view(*size[:2], -1)   # (C,N)
         # changed the order of mutiplication to avoid O(N^2) complexity
         # (x*xT)*(W*x) instead of (x*(xT*(W*x)))
         convx = self.conv(x)   # (C,C) * (C,N) = (C,N)   => O(NC^2)
-        xxT = torch.bmm(x,x.permute(0,2,1).contiguous())   # (C,N) * (N,C) = (C,C)   => O(NC^2)
+        xxT = torch.bmm(x, x.permute(0, 2, 1).contiguous())   # (C,N) * (N,C) = (C,C)   => O(NC^2)
         o = torch.bmm(xxT, convx)   # (C,C) * (C,N) = (C,N)   => O(NC^2)
         o = self.gamma * o + x
         return o.view(*size).contiguous()
@@ -90,17 +100,17 @@ class SEBlock(nn.Module):
     "se block"
     se_layer = nn.Linear
     act_fn = nn.ReLU(inplace=True)
+
     def __init__(self, c, r=16):
         super().__init__()
         ch = c // r
         self.squeeze = nn.AdaptiveAvgPool2d(1)
         self.excitation = nn.Sequential(
-            OrderedDict([
-            ('fc_reduce', self.se_layer(c, ch, bias=False)),
-            ('se_act', self.act_fn),
-            ('fc_expand', self.se_layer(ch, c, bias=False)),
-            ('sigmoid', nn.Sigmoid())
-            ]))
+            OrderedDict([('fc_reduce', self.se_layer(c, ch, bias=False)),
+                         ('se_act', self.act_fn),
+                         ('fc_expand', self.se_layer(ch, c, bias=False)),
+                         ('sigmoid', nn.Sigmoid())
+                         ]))
 
     def forward(self, x):
         bs, c, _, _ = x.shape
@@ -113,18 +123,19 @@ class SEBlockConv(nn.Module):
     "se block with conv on excitation"
     se_layer = nn.Conv2d
     act_fn = nn.ReLU(inplace=True)
+
     def __init__(self, c, r=16):
         super().__init__()
 #         c_in = math.ceil(c//r/8)*8
-        c_in = c//r
-
+        c_in = c // r
         self.squeeze = nn.AdaptiveAvgPool2d(1)
-        self.excitation = nn.Sequential(OrderedDict([
-            ('conv_reduce', self.se_layer(c, c_in, 1, bias=False)),
-            ('se_act', self.act_fn),
-            ('conv_expand', self.se_layer(c_in, c, 1, bias=False)),
-            ('sigmoid', nn.Sigmoid())
-        ]))
+        self.excitation = nn.Sequential(
+            OrderedDict([
+                ('conv_reduce', self.se_layer(c, c_in, 1, bias=False)),
+                ('se_act', self.act_fn),
+                ('conv_expand', self.se_layer(c_in, c, 1, bias=False)),
+                ('sigmoid', nn.Sigmoid())
+            ]))
 
     def forward(self, x):
         y = self.squeeze(x)
--- model_constructor/net.py
+++ model_constructor/net.py
@@ -4,7 +4,6 @@ __all__ = ['init_cnn', 'act_fn', 'ResBlock', 'NewResBlock', 'Net', 'xresnet34', 
 
 # Cell
 import torch.nn as nn
-import sys, torch
 from functools import partial
 from collections import OrderedDict
 from .layers import ConvLayer, noop, SEBlock, SimpleSelfAttention, Flatten
@@ -12,67 +11,86 @@ from .layers import ConvLayer, noop, SEBlock, SimpleSelfAttention, Flatten
 # Cell
 act_fn = nn.ReLU(inplace=True)
 
-def init_cnn(m):
-    if getattr(m, 'bias', None) is not None: nn.init.constant_(m.bias, 0)
-    if isinstance(m, (nn.Conv2d,nn.Linear)): nn.init.kaiming_normal_(m.weight)
-    for l in m.children(): init_cnn(l)
+
+def init_cnn(module: nn.Module):
+    if getattr(module, 'bias', None) is not None:
+        nn.init.constant_(module.bias, 0)
+    if isinstance(module, (nn.Conv2d, nn.Linear)):
+        nn.init.kaiming_normal_(module.weight)
+    for layer in module.children():
+        init_cnn(layer)
 
 # Cell
 class ResBlock(nn.Module):
     '''Resnet block'''
     se_block = SEBlock
+
     def __init__(self, expansion, ni, nh, stride=1,
                  conv_layer=ConvLayer, act_fn=act_fn, zero_bn=True, bn_1st=True,
                  pool=nn.AvgPool2d(2, ceil_mode=True), sa=False, sym=False, se=False, se_reduction=16,
                  groups=1, dw=False):
         super().__init__()
-        nf,ni = nh*expansion,ni*expansion
-        if groups != 1: groups = int(nh/groups)
-        layers  = [(f"conv_0", conv_layer(ni, nh, 3, stride=stride, act_fn=act_fn, bn_1st=bn_1st,
-                                         groups= nh if dw else groups)),
-                   (f"conv_1", conv_layer(nh, nf, 3, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
-        ] if expansion == 1 else [
-                   (f"conv_0",conv_layer(ni, nh, 1, act_fn=act_fn, bn_1st=bn_1st)),
-                   (f"conv_1",conv_layer(nh, nh, 3, stride=stride, act_fn=act_fn, bn_1st=bn_1st,
-                                         groups= nh if dw else groups)),
-                   (f"conv_2",conv_layer(nh, nf, 1, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
-        ]
-        if se: layers.append(('se', self.se_block(nf, se_reduction)))
-        if sa: layers.append(('sa', SimpleSelfAttention(nf,ks=1,sym=sym)))
+        nf, ni = nh * expansion, ni * expansion
+        if groups != 1:
+            groups = int(nh / groups)
+        if expansion == 1:
+            layers = [("conv_0", conv_layer(ni, nh, 3, stride=stride, act_fn=act_fn, bn_1st=bn_1st,
+                                            groups=nh if dw else groups)),
+                      ("conv_1", conv_layer(nh, nf, 3, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
+                      ]
+        else:
+            layers = [("conv_0", conv_layer(ni, nh, 1, act_fn=act_fn, bn_1st=bn_1st)),
+                      ("conv_1", conv_layer(nh, nh, 3, stride=stride, act_fn=act_fn, bn_1st=bn_1st,
+                                            groups=nh if dw else groups)),
+                      ("conv_2", conv_layer(nh, nf, 1, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
+                      ]
+        if se:
+            layers.append(('se', self.se_block(nf, se_reduction)))
+        if sa:
+            layers.append(('sa', SimpleSelfAttention(nf, ks=1, sym=sym)))
         self.convs = nn.Sequential(OrderedDict(layers))
-        self.pool = noop if stride==1 else pool
-        self.idconv = noop if ni==nf else conv_layer(ni, nf, 1, act=False)
-        self.act_fn =act_fn
+        self.pool = noop if stride == 1 else pool
+        self.idconv = noop if ni == nf else conv_layer(ni, nf, 1, act=False)
+        self.act_fn = act_fn
 
-    def forward(self, x): return self.act_fn(self.convs(x) + self.idconv(self.pool(x)))
+    def forward(self, x):
+        return self.act_fn(self.convs(x) + self.idconv(self.pool(x)))
 
 # Cell
 # NewResBlock now is YaResBlock - Yet Another ResNet Block! It is now at model_constructor.yaresnet.
+
+
 class NewResBlock(nn.Module):
     '''YaResnet block'''
     se_block = SEBlock
+
     def __init__(self, expansion, ni, nh, stride=1,
                  conv_layer=ConvLayer, act_fn=act_fn, zero_bn=True, bn_1st=True,
-                 pool=nn.AvgPool2d(2, ceil_mode=True), sa=False,sym=False, se=False, se_reduction=16,
+                 pool=nn.AvgPool2d(2, ceil_mode=True), sa=False, sym=False, se=False, se_reduction=16,
                  groups=1, dw=False):
         super().__init__()
-        nf,ni = nh*expansion,ni*expansion
-        if groups != 1: groups = int(nh/groups)
-        self.reduce = noop if stride==1 else pool
-        layers  = [(f"conv_0", conv_layer(ni, nh, 3, stride=1, act_fn=act_fn, bn_1st=bn_1st,
-                                          groups= nh if dw else groups)), # stride 1 !!!
-                   (f"conv_1", conv_layer(nh, nf, 3, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
-        ] if expansion == 1 else [
-                   (f"conv_0",conv_layer(ni, nh, 1, act_fn=act_fn, bn_1st=bn_1st)),
-                   (f"conv_1",conv_layer(nh, nh, 3, stride=1, act_fn=act_fn, bn_1st=bn_1st,
-                                         groups= nh if dw else groups)), # stride 1 !!!
-                   (f"conv_2",conv_layer(nh, nf, 1, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
-        ]
-        if se: layers.append(('se', self.se_block(nf, se_reduction)))
-        if sa: layers.append(('sa', SimpleSelfAttention(nf,ks=1,sym=sym)))
+        nf, ni = nh * expansion, ni * expansion
+        if groups != 1:
+            groups = int(nh / groups)
+        self.reduce = noop if stride == 1 else pool
+        if expansion == 1:
+            layers = [("conv_0", conv_layer(ni, nh, 3, stride=1, act_fn=act_fn, bn_1st=bn_1st,
+                                            groups=nh if dw else groups)),
+                      ("conv_1", conv_layer(nh, nf, 3, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
+                      ]
+        else:
+            layers = [("conv_0", conv_layer(ni, nh, 1, act_fn=act_fn, bn_1st=bn_1st)),
+                      ("conv_1", conv_layer(nh, nh, 3, stride=1, act_fn=act_fn, bn_1st=bn_1st,
+                                            groups=nh if dw else groups)),
+                      ("conv_2", conv_layer(nh, nf, 1, zero_bn=zero_bn, act=False, bn_1st=bn_1st))
+                      ]
+        if se:
+            layers.append(('se', self.se_block(nf, se_reduction)))
+        if sa:
+            layers.append(('sa', SimpleSelfAttention(nf, ks=1, sym=sym)))
         self.convs = nn.Sequential(OrderedDict(layers))
-        self.idconv = noop if ni==nf else conv_layer(ni, nf, 1, act=False)
-        self.merge =act_fn
+        self.idconv = noop if ni == nf else conv_layer(ni, nf, 1, act=False)
+        self.merge = act_fn
 
     def forward(self, x):
         o = self.reduce(x)
@@ -80,75 +98,77 @@ class NewResBlock(nn.Module):
 
 # Cell
 def _make_stem(self):
-        stem = [(f"conv_{i}", self.conv_layer(self.stem_sizes[i], self.stem_sizes[i+1],
-#                     stride=2 if i==0 else 1,
-                    stride=2 if i==self.stem_stride_on else 1,
-                    bn_layer=(not self.stem_bn_end) if i==(len(self.stem_sizes)-2) else True,
-                    act_fn=self.act_fn, bn_1st=self.bn_1st))
-                for i in range(len(self.stem_sizes)-1)]
-        stem.append(('stem_pool', self.stem_pool))
-        if self.stem_bn_end: stem.append(('norm', self.norm(self.stem_sizes[-1])))
-        return nn.Sequential(OrderedDict(stem))
+    stem = [(f"conv_{i}", self.conv_layer(self.stem_sizes[i], self.stem_sizes[i + 1],
+                                          stride=2 if i == self.stem_stride_on else 1,
+                                          bn_layer=(not self.stem_bn_end) if i == (len(self.stem_sizes) - 2) else True,
+                                          act_fn=self.act_fn, bn_1st=self.bn_1st))
+            for i in range(len(self.stem_sizes) - 1)]
+    stem.append(('stem_pool', self.stem_pool))
+    if self.stem_bn_end:
+        stem.append(('norm', self.norm(self.stem_sizes[-1])))
+    return nn.Sequential(OrderedDict(stem))
 
 # Cell
-def _make_layer(self,expansion,ni,nf,blocks,stride,sa):
-        return nn.Sequential(OrderedDict(
-            [(f"bl_{i}", self.block(expansion, ni if i==0 else nf, nf,
-                    stride if i==0 else 1, sa=sa if i==blocks-1 else False,
-                    conv_layer=self.conv_layer, act_fn=self.act_fn, pool=self.pool,
-                    zero_bn=self.zero_bn, bn_1st=self.bn_1st, groups=self.groups, dw=self.dw, se=self.se))
-              for i in range(blocks)]))
+def _make_layer(self, expansion, ni, nf, blocks, stride, sa):
+    layers = [(f"bl_{i}", self.block(expansion, ni if i == 0 else nf, nf,
+                                     stride if i == 0 else 1, sa=sa if i == blocks - 1 else False,
+                                     conv_layer=self.conv_layer, act_fn=self.act_fn, pool=self.pool,
+                                     zero_bn=self.zero_bn, bn_1st=self.bn_1st, groups=self.groups,
+                                     dw=self.dw, se=self.se))
+              for i in range(blocks)]
+    return nn.Sequential(OrderedDict(layers))
 
 # Cell
 def _make_body(self):
-        blocks = [(f"l_{i}", self._make_layer(self,self.expansion,
-                        self.block_sizes[i], self.block_sizes[i+1], l,
-                        1 if i==0 else 2, self.sa if i==0 else False))
-                  for i,l in enumerate(self.layers)]
-        return nn.Sequential(OrderedDict(blocks))
+    blocks = [(f"l_{i}", self._make_layer(self, self.expansion,
+                                          ni=self.block_sizes[i], nf=self.block_sizes[i + 1],
+                                          blocks=l, stride=1 if i == 0 else 2,
+                                          sa=self.sa if i == 0 else False))
+              for i, l in enumerate(self.layers)]
+    return nn.Sequential(OrderedDict(blocks))
 
 # Cell
 def _make_head(self):
-        head = [('pool', nn.AdaptiveAvgPool2d(1)),
-                ('flat', Flatten()),
-                ('fc',   nn.Linear(self.block_sizes[-1]*self.expansion, self.c_out))]
-        return nn.Sequential(OrderedDict(head))
+    head = [('pool', nn.AdaptiveAvgPool2d(1)),
+            ('flat', Flatten()),
+            ('fc', nn.Linear(self.block_sizes[-1] * self.expansion, self.c_out))]
+    return nn.Sequential(OrderedDict(head))
 
 # Cell
 class Net():
     """Model constructor. As default - xresnet18"""
     def __init__(self, name='Net', c_in=3, c_out=1000,
-                block=ResBlock, conv_layer = ConvLayer,
-                block_sizes = [64,128,256,512], layers=[2,2,2,2],
-                norm = nn.BatchNorm2d,
-                act_fn=nn.ReLU(inplace=True),
-                pool = nn.AvgPool2d(2, ceil_mode=True),
-                expansion=1, groups = 1, dw=False,
-                sa=False, se=False, se_reduction=16,
-                bn_1st = True,
-                zero_bn=True,
-                stem_stride_on = 0,
-                stem_sizes = [32,32,64],
-                stem_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-                stem_bn_end = False,
-
-                _init_cnn = init_cnn,
-                _make_stem = _make_stem,
-                _make_layer = _make_layer,
-                _make_body = _make_body,
-                _make_head = _make_head,
-                ):
+                 block=ResBlock, conv_layer=ConvLayer,
+                 block_sizes=[64, 128, 256, 512], layers=[2, 2, 2, 2],
+                 norm=nn.BatchNorm2d,
+                 act_fn=nn.ReLU(inplace=True),
+                 pool=nn.AvgPool2d(2, ceil_mode=True),
+                 expansion=1, groups=1, dw=False,
+                 sa=False, se=False, se_reduction=16,
+                 bn_1st=True,
+                 zero_bn=True,
+                 stem_stride_on=0,
+                 stem_sizes=[32, 32, 64],
+                 stem_pool=nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+                 stem_bn_end=False,
+                 _init_cnn=init_cnn,
+                 _make_stem=_make_stem,
+                 _make_layer=_make_layer,
+                 _make_body=_make_body,
+                 _make_head=_make_head,
+                 ):
         super().__init__()
 
         params = locals()
         del params['self']
         self.__dict__ = params
         self._block_sizes = params['block_sizes']
-        if self.stem_sizes[0] != self.c_in: self.stem_sizes = [self.c_in] + self.stem_sizes
+        if self.stem_sizes[0] != self.c_in:
+            self.stem_sizes = [self.c_in] + self.stem_sizes
 
     @property
     def block_sizes(self):
-        return [self.stem_sizes[-1]//self.expansion] + self._block_sizes +[256]*(len(self.layers)-4)
+        return [self.stem_sizes[-1] // self.expansion] + self._block_sizes + [256] * (len(self.layers) - 4)
 
     @property
     def stem(self):
@@ -168,18 +188,18 @@ class Net():
             ('body', self.body),
             ('head', self.head)]))
         self._init_cnn(model)
-        model.extra_repr = lambda : f"model {self.name}"
+        model.extra_repr = lambda: f"model {self.name}"
         return model
 
     def __repr__(self):
         return (f"{self.name} constructor\n"
-        f"  c_in: {self.c_in}, c_out: {self.c_out}\n"
-        f"  expansion: {self.expansion}, groups: {self.groups}, dw: {self.dw}\n"
-        f"  sa: {self.sa}, se: {self.se}\n"
-        f"  stem sizes: {self.stem_sizes}, stide on {self.stem_stride_on}\n"
-        f"  body sizes {self._block_sizes}\n"
-        f"  layers: {self.layers}")
+                f"  c_in: {self.c_in}, c_out: {self.c_out}\n"
+                f"  expansion: {self.expansion}, groups: {self.groups}, dw: {self.dw}\n"
+                f"  sa: {self.sa}, se: {self.se}\n"
+                f"  stem sizes: {self.stem_sizes}, stide on {self.stem_stride_on}\n"
+                f"  body sizes {self._block_sizes}\n"
+                f"  layers: {self.layers}")
 
 # Cell
-xresnet34 = partial(Net, name='xresnet34', expansion=1, layers = [3, 4, 6, 3])
-xresnet50 = partial(Net, name='xresnet34', expansion=4, layers = [3, 4, 6, 3])
+xresnet34 = partial(Net, name='xresnet34', expansion=1, layers=[3, 4, 6, 3])
+xresnet50 = partial(Net, name='xresnet34', expansion=4, layers=[3, 4, 6, 3])
