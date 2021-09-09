0bf76ae984e1f0de1671208e51316cc27c51eb37
ayasyrev <a.yasyrev@gmail.com>
2021-09-09 07:08:28 +0000

    typo

--- model_constructor/net.py
+++ model_constructor/net.py
@@ -196,7 +196,7 @@ class Net():
     def __repr__(self):
         return (f"{self.name} constructor\n"
                 f"  c_in: {self.c_in}, c_out: {self.c_out}\n"
-                f"  expansion: {self.expansion}, groups: {self.groups}, dw: {self.dw}\n, div_groups: {self.div_groups}"
+                f"  expansion: {self.expansion}, groups: {self.groups}, dw: {self.dw}, div_groups: {self.div_groups}\n"
                 f"  sa: {self.sa}, se: {self.se}\n"
                 f"  stem sizes: {self.stem_sizes}, stride on {self.stem_stride_on}\n"
                 f"  body sizes {self._block_sizes}\n"
