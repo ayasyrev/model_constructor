194a2b879e3de9642fca965c011a70ecef1d32e4
ayasyrev <a.yasyrev@gmail.com>
2022-04-15 18:46:20 +0300

    bump 0.1.10

--- model_constructor/__init__.py
+++ model_constructor/__init__.py
@@ -2,4 +2,4 @@ from model_constructor.model_constructor import ModelConstructor, ResBlock  # no
 from model_constructor.convmixer import ConvMixer  # noqa F401
 
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
