9b5b91f559f138527d2bf92679ee7c6b8f30efe5
ayasyrev <a.yasyrev@gmail.com>
2022-05-13 13:52:25 +0300

    setup change

--- Makefile
+++ Makefile
@@ -1,37 +1,11 @@
 .ONESHELL:
 SHELL := /bin/bash
-SRC = $(wildcard Nbs/*.ipynb)
-
-all: model_constructor docs
-
-model_constructor: $(SRC)
-	nbdev_build_lib
-	touch model_constructor
-
-sync:
-	nbdev_update_lib
-
-docs_serve: docs
-	cd docs && bundle exec jekyll serve
-
-docs: $(SRC)
-	nbdev_build_docs
-	touch docs
-
-test:
-	nbdev_test_nbs
-
-release: pypi conda_release
-	nbdev_bump_version
-
-conda_release:
-	fastrelease_conda_package
 
 pypi: dist
 	twine upload --repository pypi dist/*
 
 dist: clean
-	python setup.py sdist bdist_wheel
+	python3 -m build 
 
 clean:
 	rm -rf dist
--- setup.py
+++ setup.py
@@ -1,50 +1,36 @@
-from pkg_resources import parse_version
-from configparser import ConfigParser
-import setuptools
-assert parse_version(setuptools.__version__)>=parse_version('36.2')
+from setuptools import setup
 
-# note: all settings are in settings.ini; edit there, not here
-config = ConfigParser(delimiters=['='])
-config.read('settings.ini')
-cfg = config['DEFAULT']
 
-cfg_keys = 'version description keywords author author_email'.split()
-expected = cfg_keys + "lib_name user branch license status min_python audience language".split()
-for o in expected: assert o in cfg, "missing expected setting: {}".format(o)
-setup_cfg = {o:cfg[o] for o in cfg_keys}
+VERSION_FILENAME = 'model_constructor/version.py'
+REQUIREMENTS_FILENAME = 'requirements.txt'
+REQUIREMENTS_TEST_FILENAME = 'requirements_test.txt'
 
-licenses = {
-    'apache2': ('Apache Software License 2.0','OSI Approved :: Apache Software License'),
-    'mit': ('MIT License', 'OSI Approved :: MIT License'),
-    'gpl2': ('GNU General Public License v2', 'OSI Approved :: GNU General Public License v2 (GPLv2)'),
-    'gpl3': ('GNU General Public License v3', 'OSI Approved :: GNU General Public License v3 (GPLv3)'),
-    'bsd3': ('BSD License', 'OSI Approved :: BSD License'),
-}
-statuses = [ '1 - Planning', '2 - Pre-Alpha', '3 - Alpha',
-    '4 - Beta', '5 - Production/Stable', '6 - Mature', '7 - Inactive' ]
-py_versions = '2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8'.split()
 
-requirements = cfg.get('requirements','').split()
-min_python = cfg['min_python']
-lic = licenses.get(cfg['license'].lower(), (cfg['license'], None))
+# Requirements
+try:
+    with open(REQUIREMENTS_FILENAME, encoding="utf-8") as fh:
+        REQUIRED = fh.read().split("\n")
+except FileNotFoundError:
+    REQUIRED = []
 
-setuptools.setup(
-    name = cfg['lib_name'],
-    license = lic[0],
-    classifiers = [
-        'Development Status :: ' + statuses[int(cfg['status'])],
-        'Intended Audience :: ' + cfg['audience'].title(),
-        'Natural Language :: ' + cfg['language'].title(),
-    ] + ['Programming Language :: Python :: '+o for o in py_versions[py_versions.index(min_python):]] + (['License :: ' + lic[1] ] if lic[1] else []),
-    url = cfg['git_url'],
-    packages = setuptools.find_packages(),
-    include_package_data = True,
-    install_requires = requirements,
-    dependency_links = cfg.get('dep_links','').split(),
-    python_requires  = '>=' + cfg['min_python'],
-    long_description = open('README.md').read(),
-    long_description_content_type = 'text/markdown',
-    zip_safe = False,
-    entry_points = { 'console_scripts': cfg.get('console_scripts','').split() },
-    **setup_cfg)
+try:
+    with open(REQUIREMENTS_TEST_FILENAME, encoding="utf-8") as fh:
+        TEST_REQUIRED = fh.read().split("\n")
+except FileNotFoundError:
+    TEST_REQUIRED = []
 
+# What packages are optional?
+EXTRAS = {"test": TEST_REQUIRED}
+
+# Load the package's __version__ from version.py
+version = {}
+with open(VERSION_FILENAME, 'r', encoding="utf-8") as fh:
+    exec(fh.read(), version)
+VERSION = version['__version__']
+
+
+setup(
+    version=VERSION,
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+)
