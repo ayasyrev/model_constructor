08f54ae2ab06d2be393a0dd4fee03757f2f8eb81
ayasyrev <a.yasyrev@gmail.com>
2022-11-16 11:37:47 +0300

    struct

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 
-VERSION_FILENAME = 'model_constructor/version.py'
 REQUIREMENTS_FILENAME = 'requirements.txt'
 REQUIREMENTS_TEST_FILENAME = 'requirements_test.txt'
 
@@ -22,15 +21,8 @@ except FileNotFoundError:
 # What packages are optional?
 EXTRAS = {"test": TEST_REQUIRED}
 
-# Load the package's __version__ from version.py
-version = {}
-with open(VERSION_FILENAME, 'r', encoding="utf-8") as fh:
-    exec(fh.read(), version)
-VERSION = version['__version__']
-
 
 setup(
-    version=VERSION,
     install_requires=REQUIRED,
     extras_require=EXTRAS,
 )
