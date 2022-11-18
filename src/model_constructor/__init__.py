9d9e3f976e67fc17370281575ab775938c1cf75b
ayasyrev <a.yasyrev@gmail.com>
2022-11-18 10:53:36 +0300

    docs after cfg

--- src/model_constructor/__init__.py
+++ src/model_constructor/__init__.py
@@ -1,4 +1,4 @@
 from model_constructor.convmixer import ConvMixer  # noqa F401
-from model_constructor.model_constructor import ModelConstructor, ResBlock # noqa F401
+from model_constructor.model_constructor import ModelConstructor, ResBlock, CfgMC # noqa F401
                                                  
 from model_constructor.version import __version__  # noqa F401
