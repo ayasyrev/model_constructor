2cdd6a0766205393dd0ffab530e3abfdc8f2f28a
ayasyrev <a.yasyrev@gmail.com>
2022-12-23 11:07:42 +0300

    move old models

--- src/model_constructor/base_constructor.py
+++ src/model_constructor/base_constructor.py
@@ -1,10 +1,31 @@
-import torch.nn as nn
+"""First version of constructor.
+"""
+# Used in examples.
+# first implementation of xresnet - inspired by fastai version.
 from collections import OrderedDict
-from .layers import ConvLayer, Noop, Flatten
+from functools import partial
 
+import torch.nn as nn
 
-__all__ = ['act_fn', 'Stem', 'DownsampleBlock', 'BasicBlock', 'Bottleneck', 'BasicLayer', 'Body', 'Head', 'init_model',
-           'Net']
+from .layers import ConvLayer, Flatten, Noop
+
+__all__ = [
+    "act_fn",
+    "Stem",
+    "DownsampleBlock",
+    "BasicBlock",
+    "Bottleneck",
+    "BasicLayer",
+    "Body",
+    "Head",
+    "init_model",
+    "Net",
+    "DownsampleLayer",
+    "XResBlock",
+    "xresnet18",
+    "xresnet34",
+    "xresnet50",
+]
 
 
 act_fn = nn.ReLU(inplace=True)
@@ -162,3 +183,58 @@ class Net(nn.Sequential):
              ('head', head(body_out * expansion, num_classes, **kwargs))
              ]))
         self.init_model(self)
+
+
+# xresnet from fastai
+
+
+class DownsampleLayer(nn.Sequential):
+    """Downsample layer for Xresnet Resblock"""
+
+    def __init__(self, conv_layer, ni, nf, stride, act,
+                 pool=nn.AvgPool2d(2, ceil_mode=True), pool_1st=True,
+                 **kwargs):
+        layers = [] if stride == 1 else [('pool', pool)]
+        layers += [] if ni == nf else [('idconv', conv_layer(ni, nf, 1, act=act, **kwargs))]
+        if not pool_1st:
+            layers.reverse()
+        super().__init__(OrderedDict(layers))
+
+
+class XResBlock(nn.Module):
+    '''XResnet block'''
+
+    def __init__(self, ni, nh, expansion=1, stride=1, zero_bn=True,
+                 conv_layer=ConvLayer, act_fn=act_fn, **kwargs):
+        super().__init__()
+        nf, ni = nh * expansion, ni * expansion
+        layers = [('conv_0', conv_layer(ni, nh, 3, stride=stride, act_fn=act_fn, **kwargs)),
+                  ('conv_1', conv_layer(nh, nf, 3, zero_bn=zero_bn, act=False, act_fn=act_fn, **kwargs))
+                  ] if expansion == 1 else [
+                      ('conv_0', conv_layer(ni, nh, 1, act_fn=act_fn, **kwargs)),
+                      ('conv_1', conv_layer(nh, nh, 3, stride=stride, act_fn=act_fn, **kwargs)),
+                      ('conv_2', conv_layer(nh, nf, 1, zero_bn=zero_bn, act=False, act_fn=act_fn, **kwargs))
+        ]
+        self.convs = nn.Sequential(OrderedDict(layers))
+        self.identity = DownsampleLayer(conv_layer, ni, nf, stride,
+                                        act=False, act_fn=act_fn, **kwargs) if ni != nf or stride == 2 else Noop()
+        self.merge = Noop()
+        self.act_fn = act_fn
+
+    def forward(self, x):
+        return self.act_fn(self.merge(self.convs(x) + self.identity(x)))
+
+
+def xresnet18(**kwargs):
+    """Constructs xresnet18 model. """
+    return Net(stem_sizes=[32, 32], block=XResBlock, blocks=[2, 2, 2, 2], expansion=1, **kwargs)
+
+
+def xresnet34(**kwargs):
+    """Constructs xresnet34 model. """
+    return Net(stem_sizes=[32, 32], block=XResBlock, blocks=[3, 4, 6, 3], expansion=1, **kwargs)
+
+
+def xresnet50(**kwargs):
+    """Constructs xresnet50 model. """
+    return Net(stem_sizes=[32, 32], block=XResBlock, blocks=[3, 4, 6, 3], expansion=4, **kwargs)
