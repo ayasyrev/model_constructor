4971b4d20a9a84b960b226bdc9803b62ee801af2
ayasyrev <a.yasyrev@gmail.com>
2024-01-09 12:48:07 +0000

    nb mxresnet

--- src/model_constructor/mxresnet.py
+++ src/model_constructor/mxresnet.py
@@ -1,4 +1,5 @@
-from typing import List
+from functools import partial
+from typing import List, Optional
 
 from torch import nn
 
@@ -10,6 +11,8 @@ from .xresnet import McXResNet
 class McMxResNet(McXResNet):
     stem_sizes: List[int] = [3, 32, 64, 64]
     act_fn: nnModule = nn.Mish
+    pool: Optional[nnModule] = partial(nn.AvgPool2d, kernel_size=2, ceil_mode=True)
+
 
 
 class McMxResNet34(McMxResNet):
