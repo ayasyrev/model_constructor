c17ddf3ba73a6f0cc45e293b0ef391e66a688136
ayasyrev <a.yasyrev@gmail.com>
2024-07-19 08:33:00 +0000

    fix nox files

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,8 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"], venv_backend="uv")
 def tests(session: nox.Session) -> None:
     args = session.posargs or ["--cov"]
-    session.install("uv")
-    session.run("uv", "pip", "install", ".[test]")
-    session.run("pytest", external=True, *args)
+    session.install("-e .[test]")
+    session.run("pytest", *args)
