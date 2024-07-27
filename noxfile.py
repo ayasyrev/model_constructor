fe0c894c63837013ce628d606afc5bc81273506e
ayasyrev <a.yasyrev@gmail.com>
2024-07-27 08:32:40 +0000

    update setup to python 3.12

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"], venv_backend="uv")
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="uv")
 def tests(session: nox.Session) -> None:
     args = session.posargs or ["--cov"]
     session.install("-e .[test]")
--- noxfile_conda_lint.py
+++ noxfile_conda_lint.py
@@ -3,7 +3,7 @@ import nox
 locations = "."
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"], venv_backend="mamba")
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="mamba")
 def conda_lint(session: nox.Session) -> None:
     args = session.posargs or locations
     session.conda_install("ruff")
